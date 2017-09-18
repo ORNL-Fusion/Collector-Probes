908a8d2d33938d03f9e6a4b1e35bf96fb60ce509
Unterberg <unterberge@fusion.gat.com>
2017-09-18 07:13:20 -0700

    New version is from my Sandbox area.

--- EFIT_xsec_DP.py
+++ EFIT_xsec_DP.py
@@ -2,6 +2,7 @@ import numpy as np
 import pylab as plt
 import scipy.interpolate as scinter
 
+import MDSplus as mds
 import DIIID.plot_vv_coils as pvc
 import EFIT.load_gfile_d3d as loadg
 
@@ -11,8 +12,8 @@ def plot_EFIT_helper(shot, time, probe_tip, levs=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 
                      plotIT=False, server='atlas.gat.com'):
 
     # gNAM = 'g'+str(shot)+'.0'+str(time)
-
-    parmDICT = loadg.read_g_file_mds(shot, time, tree=tree, Server=server)
+    MDSplusCONN = mds.Connection(server)
+    parmDICT = loadg.read_g_file_mds(shot, time, tree=tree, connection=MDSplusCONN)
     Rs, Zs = np.meshgrid(parmDICT['R'], parmDICT['Z'])
     Z_axis = parmDICT['ZmAxis']
     R_axis = parmDICT['RmAxis']
@@ -24,31 +25,34 @@ def plot_EFIT_helper(shot, time, probe_tip, levs=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 
 
     # translate up to outbard midplane
     Rs_trunc = Rs > R_axis
-    f_psiN = scinter.interp2d(Rs[Rs_trunc], Zs[Rs_trunc], parmDICT['psiRZn'][Rs_trunc])
+    f_psiN = scinter.Rbf(Rs[Rs_trunc], Zs[Rs_trunc], parmDICT['psiRZn'][Rs_trunc],
+                         function='linear')
     f_Romp = scinter.interp2d(parmDICT['psiRZn'][Rs_trunc], Zs[Rs_trunc], Rs[Rs_trunc])
-
     # remap each array from probe location to mag. axis midplane
-    psiN_AU = f_psiN([2.26, 2.27], -0.18)
-    # psiN_B = f_psiN(Rs, -0.1546)
-    # psiN_C = f_psiN(Rs, -0.2054)
+    alpha_A = 0.521 / 100.0
+    alpha_B = 0.137 / 100.0
+    alpha_C = 0.076 / 100.0
+    lamb = 1.27 / 100.0
+    psiN_A = f_psiN(probe_tip+alpha_A, -0.18)
+    psiN_B = f_psiN(probe_tip+lamb+alpha_B, -0.1546)
+    psiN_C = f_psiN(probe_tip+lamb+alpha_C, -0.2054)
 
-    R_omp_AU = f_Romp(psiN_AU, Z_axis)
-
-    print f_Rs(Z_axis)
-    print psiN_AU
-    print R_omp_AU
-    print "A probe R_sep: {:.6}".format(f_Rs(-0.18))
-    print "B probe R_sep: {:.6}".format(f_Rs(-0.1546))
-    print "C probe R_sep: {:.6}".format(f_Rs(-0.2054))
+    R_omp_A = f_Romp(psiN_A, Z_axis)
+    R_omp_B = f_Romp(psiN_B, Z_axis)
+    R_omp_C = f_Romp(psiN_C, Z_axis)
+    print "R_sep_OMP: {:.6}".format(f_Rs(parmDICT['ZmAxis']))
+    print "A probe R_sep_OMP: {:.6}".format(R_omp_A[0])
+    print "B probe R_sep_OMP: {:.6}".format(R_omp_B[0])
+    print "C probe R_sep_OMP: {:.6}".format(R_omp_C[0])
 
     if plotIT:
         plt.figure(figsize=(7, 10), facecolor='w')
         plt.subplot(111, aspect='equal')
         plt.axis([0.9, 2.5, -1.5, 1.5])
 
-        plt.plot([probe_tip, 2.365], [-0.18, -0.18], 'r', linewidth=7)
-        plt.plot([probe_tip+0.01, 2.365], [-0.1546, -0.1546], 'r', linewidth=3)
-        plt.plot([probe_tip+0.01, 2.365], [-0.2054, -0.2054], 'r', linewidth=2)
+        plt.plot([probe_tip, 2.365], [-0.18, -0.18], 'r', linewidth=12)
+        plt.plot([probe_tip+0.0127, 2.365], [-0.1546, -0.1546], 'orange', linewidth=4)
+        plt.plot([probe_tip+0.0127, 2.365], [-0.2054, -0.2054], 'green', linewidth=2)
 
         plt.plot(parmDICT['wall'][:, 0], parmDICT['wall'][:, 1], 'k', linewidth=3)
         plt.plot(pvc.RVVIN, pvc.ZVVIN, 'k', linewidth=1)
@@ -59,6 +63,12 @@ def plot_EFIT_helper(shot, time, probe_tip, levs=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 
         plt.contour(Rs, Zs, parmDICT['psiRZn'], [0.9996], colors='b', linewidths=2)
         plt.plot(parmDICT['lcfs'][:, 0], parmDICT['lcfs'][:, 1], 'b', linewidth=2)
         plt.plot(parmDICT['RmAxis'], parmDICT['ZmAxis'], 'b', linewidth=1, marker='+',
+                 markersize=10)
+        plt.plot(R_omp_A, parmDICT['ZmAxis'], 'red', linewidth=0.1, marker='o',
+                 markersize=10)
+        plt.plot(R_omp_B, parmDICT['ZmAxis'], 'orange', linewidth=0.1, marker='o',
+                 markersize=10)
+        plt.plot(R_omp_C, parmDICT['ZmAxis'], 'green', linewidth=0.1, marker='o',
                  markersize=10)
         plt.show()
 

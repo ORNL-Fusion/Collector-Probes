c7586fb8152cf6a5309d82e21d971f79023c6c7e
Unknown <shawnzamperini@gmail.com>
2018-04-12 15:47:23 -0400

    Made gfile pull function in script. No more EFIT repository.

--- get_Rsep.py
+++ get_Rsep.py
@@ -8,8 +8,119 @@ import scipy.interpolate as scinter
 
 # Normal load_gfile_d3d except needed to cast 'g['lcfs']' as an int.
 import MDSplus as mds
-import EFIT.load_gfile_d3d as loadg
+#import EFIT.load_gfile_d3d as loadg
 import meas_locations as geo
+
+
+# Copy and pasted from the EFOT repository for convienence.
+def read_g_file_mds(shot, time, tree='EFIT01', exact=False, connection=None,
+                    write2file=True, gpath='.'):
+    import MDSplus
+
+    # in case those are passed in as strings
+    shot = int(shot)
+    time = int(time)
+
+    # Connect to server, open tree and go to g-file
+    if connection is None:
+        connection = MDSplus.Connection('atlas.gat.com')
+    connection.openTree(tree, shot)
+    base = 'RESULTS:GEQDSK:'
+
+    # get time slice
+    signal = 'GTIME'
+    k = np.argmin(np.abs(connection.get(base + signal).data() - time))
+    time0 = int(connection.get(base + signal).data()[k])
+
+    if (time != time0):
+        if exact:
+            raise RuntimeError(tree + ' does not exactly contain time ' + str(time) + '  ->  Abort')
+        else:
+            print('Warning: ' + tree + ' does not exactly contain time ' + str(time) + ' the closest time is ' + str(time0))
+            print('Fetching time slice ' + str(time0))
+            time = time0
+
+    # store data in dictionary
+    g = {'shot': shot, 'time': time}
+
+    # get header line
+    header = connection.get(base + 'ECASE').data()[k]
+
+    # get all signals, use same names as in read_g_file
+    translate = {'MW': 'NR', 'MH': 'NZ', 'XDIM': 'Xdim', 'ZDIM': 'Zdim', 'RZERO': 'R0',
+                 'RMAXIS': 'RmAxis', 'ZMAXIS': 'ZmAxis', 'SSIMAG': 'psiAxis', 'SSIBRY': 'psiSep',
+                 'BCENTR': 'Bt0', 'CPASMA': 'Ip', 'FPOL': 'Fpol', 'PRES': 'Pres',
+                 'FFPRIM': 'FFprime', 'PPRIME': 'Pprime', 'PSIRZ': 'psiRZ', 'QPSI': 'qpsi',
+                 'NBBBS': 'Nlcfs', 'LIMITR': 'Nwall'}
+    for signal in translate:
+        g[translate[signal]] = connection.get(base + signal).data()[k]
+
+    g['R1'] = connection.get(base + 'RGRID').data()[0]
+    g['Zmid'] = 0.0
+
+    RLIM = connection.get(base + 'LIM').data()[:, 0]
+    ZLIM = connection.get(base + 'LIM').data()[:, 1]
+    g['wall'] = np.vstack((RLIM, ZLIM)).T
+
+    RBBBS = connection.get(base + 'RBBBS').data()[k][:int(g['Nlcfs'])]
+    ZBBBS = connection.get(base + 'ZBBBS').data()[k][:int(g['Nlcfs'])]
+    g['lcfs'] = np.vstack((RBBBS, ZBBBS)).T
+
+    KVTOR = 0
+    RVTOR = 1.7
+    NMASS = 0
+    RHOVN = connection.get(base + 'RHOVN').data()[k]
+
+    # convert floats to integers
+    for item in ['NR', 'NZ', 'Nlcfs', 'Nwall']:
+        g[item] = int(g[item])
+
+    # convert single (float32) to double (float64) and round
+    for item in ['Xdim', 'Zdim', 'R0', 'R1', 'RmAxis', 'ZmAxis', 'psiAxis', 'psiSep', 'Bt0', 'Ip']:
+        g[item] = np.round(np.float64(g[item]), 7)
+
+    # convert single arrays (float32) to double arrays (float64)
+    for item in ['Fpol', 'Pres', 'FFprime', 'Pprime', 'psiRZ', 'qpsi', 'lcfs', 'wall']:
+        g[item] = np.array(g[item], dtype=np.float64)
+
+    # write g-file to disk
+    if write2file:
+        if not (gpath[-1] == '/'): gpath += '/'
+        with open(gpath + 'g' + format(shot,'06d') + '.' + format(time,'05d'), 'w') as f:
+            if ('EFITD' in header[0]) and (len(header) == 6):
+                for item in header: f.write(item)
+            else:
+                f.write('  EFITD    xx/xx/xxxx    #' + str(shot) + '  ' + str(time) + 'ms        ')
+
+            f.write('   3 ' + str(g['NR']) + ' ' + str(g['NZ']) + '\n')
+            f.write('% .9E% .9E% .9E% .9E% .9E\n'%(g['Xdim'], g['Zdim'], g['R0'], g['R1'], g['Zmid']))
+            f.write('% .9E% .9E% .9E% .9E% .9E\n'%(g['RmAxis'], g['ZmAxis'], g['psiAxis'], g['psiSep'], g['Bt0']))
+            f.write('% .9E% .9E% .9E% .9E% .9E\n'%(g['Ip'], 0, 0, 0, 0))
+            f.write('% .9E% .9E% .9E% .9E% .9E\n'%(0,0,0,0,0))
+            write_array(g['Fpol'], f)
+            write_array(g['Pres'], f)
+            write_array(g['FFprime'], f)
+            write_array(g['Pprime'], f)
+            write_array(g['psiRZ'].flatten(), f)
+            write_array(g['qpsi'], f)
+            f.write(str(g['Nlcfs']) + ' ' + str(g['Nwall']) + '\n')
+            write_array(g['lcfs'].flatten(), f)
+            write_array(g['wall'].flatten(), f)
+            f.write(str(KVTOR) + ' ' + format(RVTOR, ' .9E') + ' ' + str(NMASS) + '\n')
+            write_array(RHOVN, f)
+
+    # Construct (R,Z) grid for psiRZ
+    g['dR'] = g['Xdim']/(g['NR'] - 1)
+    g['R'] = g['R1'] + np.arange(g['NR'])*g['dR']
+
+    g['dZ'] = g['Zdim']/(g['NZ'] - 1)
+    NZ2 = int(np.floor(0.5*g['NZ']))
+    g['Z'] = g['Zmid'] + np.arange(-NZ2, NZ2+1)*g['dZ']
+
+    # normalize psiRZ
+    g['psiRZn'] = (g['psiRZ'] - g['psiAxis']) / (g['psiSep'] - g['psiAxis'])
+
+    return g
 
 
 # Return dictionary of average R - Rsep value for each of the probes.
@@ -77,7 +188,11 @@ def avg_Rsep_all(shots, r_probe, locations, writeToFile=False,
             print("] " + str(int(count/total_runs*100.0))+"% \n\n")
 
             # Lines from Zeke's code.
-            parmDICT = loadg.read_g_file_mds(shot, time,
+            #parmDICT = loadg.read_g_file_mds(shot, time,
+            #                                 connection=MDSplusCONN,
+            #                                 write2file=False,
+            #                                 tree=Etree)
+            parmDICT = read_g_file_mds(shot, time,
                                              connection=MDSplusCONN,
                                              write2file=False,
                                              tree=Etree)
